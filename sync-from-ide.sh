76e9effb4be078aae8342ad3a6aa5828fc04e88b
Max Zheng <maxzheng.os@gmail.com>
2018-07-02 23:20:43 -0700

    Strip out last few lines from bashrc

--- sync-from-ide.sh
+++ sync-from-ide.sh
@@ -6,4 +6,12 @@ docker cp ide:/home/$USER/.bashrc user/.bashrc
 docker cp ide:/home/$USER/.vimrc user/.vimrc
 docker cp ide:/home/$USER/.vim/python.vim user/.vim/python.vim
 
+(set +x
+# Strip out last few lines added by Dockerfile
+lines=`cat user/.bashrc | wc -l`
+let lines=lines-5
+head -n $lines user/.bashrc > /tmp/.bashrc
+mv /tmp/.bashrc user/.bashrc
+) 2> /dev/null
+
 git diff
