024659a005b4ec9a9def8214a9172d95d9253bed
Max Zheng <maxzheng.os@gmail.com>
2019-10-30 22:05:50 -0700

    Update .gitignore and copy/sync .inputrc

--- sync-from-ide.sh
+++ sync-from-ide.sh
@@ -3,6 +3,8 @@
 set -x
 
 docker cp ide:/home/$USER/.bashrc user/.bashrc
+docker cp ide:/home/$USER/.inputrc user/.inputrc
+docker cp ide:/home/$USER/.gitignore user/.gitignore
 docker cp ide:/home/$USER/.vimrc user/.vimrc
 docker cp ide:/home/$USER/.vim/python.vim user/.vim/python.vim
 docker cp ide:/home/$USER/bin/ user/
